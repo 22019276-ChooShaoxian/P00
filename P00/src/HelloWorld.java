1ce08389f7248144af05cb8c53859ef5d06be8ac
halfnhalf224 <halfnhalf224gaming@gmail.com>
2023-05-29 10:45:23 +0800

    added test

--- P00/src/HelloWorld.java
+++ P00/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Not my first Java Program");
+		System.out.println("TEST");
 	}
 
 }
